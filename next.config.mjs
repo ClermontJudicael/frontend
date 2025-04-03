f0e6729af4b6a6bde1e8ba2acbd532004d18d210
Papillon <hei.rado.3@gmail.com>
2025-04-03 18:09:10 +0300

    fix(event): add autorization to fetch image

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['localhost', 'localhost:5000'], // Autorise les images provenant de localhost et du port 5000
+    },
+  };
+  
+  export default nextConfig;
+  
